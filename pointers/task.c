319d4e1d15511d6a2de6c4ae95c763fca08a87b1
lilomox <sadesher@student.21-school.ru>
2023-06-15 16:03:57 +0700

    add third homework

---
+++ pointers/task.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 270533154;
+	char *pointer = &x;
+	
+	printf("x = %d\n", x);
+	printf("Bits of source x: ");
+	for (int i = 0; i < 4; i++) {
+		printf("%d ", pointer[i]);
+	}
+	printf("\n");
+
+	pointer[2] = 127;
+	
+	printf("Changed x = %d\n", x);
+	printf("Bits of changed x: ");
+	for (int i = 0; i < 4; i++) {
+                printf("%d ", pointer[i]);
+        }
+        printf("\n");
+}
