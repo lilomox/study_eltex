e34dcc6e32a8a7bc5ae266f93efb9e13e943f00a
lilomox <sadesher@student.21-school.ru>
2023-07-25 18:08:38 +0700

    add 18 homework

---
+++ sokets1/AF_INET/client.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define ADDR "127.0.0.2"
+#define PORT 7777
+#define SIZE 199
+
+int main() {
+	char buf[SIZE];
+
+	struct sockaddr_in server;
+	int fd;
+
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	server.sin_family = AF_INET;
+	server.sin_port = htons(PORT);
+	inet_pton(AF_INET, ADDR, &(server.sin_addr));
+
+	if (connect(fd, (struct sockaddr *)&server, sizeof(server)) == -1) {
+		perror("connect");
+		return 1;
+	}
+	char c = 's';
+	socklen_t len_cln = sizeof(server);
+	sendto(fd, &c, 1, 0, (struct sockaddr *)&server, sizeof(server));
+
+	printf("Waiting message...\n");
+
+	recvfrom(fd, buf, sizeof(buf), 0, (struct sockaddr *)&server, &len_cln);
+	printf("Recieved message: %s", buf);
+	memset(buf, '\0', SIZE);
+
+	printf("Enter message: ");
+	fgets(buf, SIZE, stdin);
+
+	sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&server, sizeof(server));
+
+	close(fd);
+}
